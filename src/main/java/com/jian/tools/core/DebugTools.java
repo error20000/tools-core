383104be0e5be85a7abd8351f51e6541bc8c6907
liujian <449667481@qq.com>
2018-01-27 07:56:19 +0800

    update

---
+++ src/main/java/com/jian/tools/core/DebugTools.java
@@ -0,0 +1,21 @@
+package com.jian.tools.core;
+
+public class DebugTools {
+	
+	
+	/**
+	 * 调试输出
+	 * @param info 输出信息
+	 * @param debug 调试开关    true System.out false log4j
+	 */
+	public static void println(String info, boolean debug){
+		if(debug) {
+			System.out.println(info);
+		}else {
+			//TODO 输出日志
+		}
+	}
+	
+	
+	
+}
